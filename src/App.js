12dac6c02385fb5d8e892ed7d9e048c46a06e8ed
Laura Kondrataite <laura.kondrataite@gmail.com>
2024-12-05 18:30:53 +0000

    setup(install bootstrap and create testing.md file)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import Button from 'react-bootstrap/Button';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <h1>testing react app</h1>
+      <Button variant="primary">Primary</Button>
     </div>
   );
 }
