f7eb458ec88c2ea5a42c77ebb3697b70b6cca130
Laura Kondrataite <laura.kondrataite@gmail.com>
2024-12-06 08:14:00 +0000

    css(set up styles folder and add temp styles to Navbar)

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,37 +1,34 @@
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import Container from 'react-bootstrap/Container';
-import React from 'react';
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import React from "react";
+import styles from "../styles/NavBar.module.css";
 
 const NavBar = () => {
     return (
-        <div>
-            <Navbar expand="md" fixed='top'>
-                <Container>
-                    <Navbar.Brand href="#home">
-                        Artly
-                        </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="ml-auto text-left">
-                            <Nav.Link>
-                                Post your Artwork
-                            </Nav.Link>
-                            <Nav.Link>
-                                Saved
-                            </Nav.Link>
-                            <Nav.Link>
-                                Sign in
-                            </Nav.Link>
-                            <Nav.Link>
-                                Sign up
-                            </Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </div>
-    )
-}
+        <Navbar className={styles.NavBar} expand="md" fixed="top">
+            <Container>
+                <Navbar.Brand href="#home">Artly</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto text-left">
+                    <Nav.Link>
+                        Post your Artwork
+                    </Nav.Link>
+                    <Nav.Link>
+                        Saved
+                    </Nav.Link>
+                    <Nav.Link>
+                        Sign in
+                    </Nav.Link>
+                    <Nav.Link>
+                        Sign up
+                    </Nav.Link>
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
 
-export default NavBar
+export default NavBar;
