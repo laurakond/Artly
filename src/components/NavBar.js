c3788072d20c2352335fa082aea39d704ca75630
Laura Kondrataite <laura.kondrataite@gmail.com>
2024-12-06 18:16:11 +0000

    components(add user context to navbar)

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,29 @@
+import React from "react";
+import { useLoggedInUser } from "../contexts/LoggedInUserContext";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
-import React from "react";
 import styles from "../styles/NavBar.module.css";
 import {NavLink} from 'react-router-dom';
 
 const NavBar = () => {
+    const loggedInUser = useLoggedInUser();
+    const loggedInIcons = (
+        <>
+            {loggedInUser?.username}
+        </>
+    )
+    const loggedOutIcons = (
+        <>
+            <NavLink className={styles.NavLink} to='/signin'>
+                Sign in
+            </NavLink>
+            <NavLink className={styles.NavLink} to='/signup'>
+                Sign up
+            </NavLink>
+        </>
+    );
+
     return (
         <Navbar className={styles.NavBar} expand="md" fixed="top">
             <Container>
@@ -23,12 +41,7 @@ const NavBar = () => {
                     <NavLink className={styles.NavLink} to='/create'>
                         Post your Artwork
                     </NavLink>
-                    <NavLink className={styles.NavLink} to='/signin'>
-                        Sign in
-                    </NavLink>
-                    <NavLink className={styles.NavLink} to='/signup'>
-                        Sign up
-                    </NavLink>
+                    {loggedInUser ? loggedInIcons : loggedOutIcons}
                 </Nav>
                 </Navbar.Collapse>
             </Container>
