08a8f58c3319488e4bd21216367e4d95e5e91257
Laura Kondrataite <laura.kondrataite@gmail.com>
2024-12-07 11:18:04 +0000

    components(add signout function)
    components(tidy up navbar)

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,48 @@
 import React from "react";
-import { useLoggedInUser } from "../contexts/LoggedInUserContext";
+import { useLoggedInUser, useSetLoggedInUser } from "../contexts/LoggedInUserContext";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import styles from "../styles/NavBar.module.css";
 import {NavLink} from 'react-router-dom';
+import axios from "axios";
+
 
 const NavBar = () => {
     const loggedInUser = useLoggedInUser();
+    const setLoggedInUser = useSetLoggedInUser();
+
+    // Function that handles signout functionality
+    const handleSignOut = async () => {
+        try {
+            await axios.post("dj-rest-auth/logout/");
+            setLoggedInUser(null);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    // Icons to show when the user is logged in
     const loggedInIcons = (
         <>
+            <NavLink className={styles.NavLink} to='artworks/create'>
+                Create Artwork
+            </NavLink>
             {loggedInUser?.username}
+            <NavLink className={styles.NavLink} to='/' onClick={handleSignOut}>
+                Sign out
+            </NavLink>
         </>
     )
+
+    // Icons to show when the user is logged out
     const loggedOutIcons = (
         <>
-            <NavLink className={styles.NavLink} to='/signin'>
-                Sign in
-            </NavLink>
             <NavLink className={styles.NavLink} to='/signup'>
                 Sign up
+            </NavLink>
+            <NavLink className={styles.NavLink} to='/signin'>
+                Sign in
             </NavLink>
         </>
     );
@@ -37,9 +60,6 @@ const NavBar = () => {
                 <Nav className="ml-auto text-left">
                     <NavLink className={styles.NavLink} to='/'>
                         Home
-                    </NavLink>
-                    <NavLink className={styles.NavLink} to='/create'>
-                        Post your Artwork
                     </NavLink>
                     {loggedInUser ? loggedInIcons : loggedOutIcons}
                 </Nav>
