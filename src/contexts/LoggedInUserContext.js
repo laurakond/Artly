9274d5127f13cab437c2d0fcf826fc080397f86b
Laura Kondrataite <laura.kondrataite@gmail.com>
2024-12-07 09:41:43 +0000

    context(write logic for access tokens)

--- src/contexts/LoggedInUserContext.js
+++ src/contexts/LoggedInUserContext.js
@@ -1,5 +1,7 @@
-import { createContext, useContext, useEffect, useState } from 'react';
+import { createContext, useContext, useEffect, useMemo, useState } from 'react';
 import axios from 'axios';
+import { axiosReq, axiosRes } from '../api/AxiosDefaults';
+import { useHistory } from 'react-router-dom';
 
 export const LoggedinUserContext = createContext();
 export const SetLoggedinUserContext = createContext();
@@ -9,19 +11,62 @@ export const useSetLoggedInUser = () => useContext(SetLoggedinUserContext);
 
 export const LoggedInUserProvider = ({children}) => {
     const [loggedInUser, setLoggedInUser] = useState(null);
+    const history = useHistory();
 
     const handleMount = async () => {
         try {
-            const {data} = await axios.get('dj-rest-auth/user/')
+            const {data} = await axiosRes.get('dj-rest-auth/user/')
             setLoggedInUser(data)
         } catch (error) {
             console.log(error);
         }
     };
-    
+
     useEffect(()=>{
         handleMount()
     }, []);
+
+    useMemo(() => {
+        axiosReq.interceptors.request.use(
+            async (config) => {
+                try {
+                await axios.post("/dj-rest-auth/token/refresh/");
+                } catch (err) {
+                setLoggedInUser((prevLoggedInUser) => {
+                    if (prevLoggedInUser) {
+                    history.push("/signin");
+                    }
+                    return null;
+                });
+                return config;
+                }
+                return config;
+            },
+            (err) => {
+                return Promise.reject(err);
+            }
+            );
+
+            axiosRes.interceptors.response.use(
+            (response) => response,
+            async (err) => {
+                if (err.response?.status === 401) {
+                try {
+                    await axios.post("/dj-rest-auth/token/refresh/");
+                } catch (err) {
+                    setLoggedInUser((prevLoggedInUser) => {
+                    if (prevLoggedInUser) {
+                        history.push("/signin");
+                    }
+                    return null;
+                    });
+                }
+                return axios(err.config);
+                }
+                return Promise.reject(err);
+            }
+            );
+        }, [history]);
 
     return(
         <LoggedinUserContext.Provider value={loggedInUser}>
