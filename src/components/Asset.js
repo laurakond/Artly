8618ae3ebdc0b978af3d05c87b0c856a76be6027
Laura Kondrataite <laura.kondrataite@gmail.com>
2024-12-09 08:54:10 +0000

    assets(correct typo)

--- src/components/Asset.js
+++ src/components/Asset.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Spinner from "react-bootstrap/Spinner";
-import styles from "../styles/LoaderAsset.module.css";
+import styles from "../styles/Asset.module.css";
 
 const Asset = ({ spinner, src, message }) => {
     return (
-        <div className={`${styles.LoaderAsset} p-4`}>
+        <div className={`${styles.Asset} p-4`}>
         {spinner && <Spinner animation="border" />}
         {src && <img src={src} alt={message} />}
         {message && <p className="mt-4">{message}</p>}
