c4ab5e0e96f268aef6f1b34ddae8933a5532d789
Laura Kondrataite <laura.kondrataite@gmail.com>
2024-12-20 17:51:20 +0000

    bids(add display functionality to show buyer's email to the seller)

--- src/pages/bids/Bid.js
+++ src/pages/bids/Bid.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useLoggedInUser } from '../../contexts/LoggedInUserContext';
 // import { Link } from 'react-router-dom/cjs/react-router-dom.min';
-// import { DropdownMenu } from '../../components/DropdownMenu';
 
 const Bid = (props) => {
     const {
@@ -9,7 +8,7 @@ const Bid = (props) => {
         updated_at,
         bid_price,
         seller,
-        // email,
+        email,
         status,
         id,
         handleAcceptBid,
@@ -23,19 +22,35 @@ const Bid = (props) => {
         <div>
             <span>{buyer} -- </span> 
             <span>{updated_at}</span>
-            <p > Bid placed: £{bid_price} Status: {status}</p>
-            {/* {is_seller && (
-                <DropdownMenu handleAcceptBid={()=>{}}
-                handleRejectBid={handleRejectBid}
-            />
-            )} */}
-            {is_seller && (
+            <p > Bid placed: £{bid_price} Status: {status} </p>
+
+            {is_seller && status==="Sold" ? (
+                <> 
+                <a 
+                    href={`mailto:${email}`}
+                    rel="noopener"
+                    aria-label=
+                    "Email the buyer (opens an email claient to choose from in new window)"
+                >
+                    Contact the buyer
+                </a>
+                <br/><br/>
+                </>
+            ): is_seller && (
                 <>
                 <button onClick={()=>handleAcceptBid(id)}>Approve</button>
                 <button onClick={()=>handleRejectBid(id)}>Reject</button>
                 <button onClick={()=>handleSoldBid(id)}>Mark as sold</button>
                 </>
             )}
+
+            {/* {is_seller && (
+                <>
+                <button onClick={()=>handleAcceptBid(id)}>Approve</button>
+                <button onClick={()=>handleRejectBid(id)}>Reject</button>
+                <button onClick={()=>handleSoldBid(id)}>Mark as sold</button>
+                </>
+            )} */}
         </div>
     )
 }
