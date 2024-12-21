264dd9f5ab077f31b2200c011116b169eef0a73c
Laura Kondrataite <laura.kondrataite@gmail.com>
2024-12-21 16:44:37 +0000

    pages(format artwork page)

--- src/pages/artworks/Artwork.js
+++ src/pages/artworks/Artwork.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import { useLoggedInUser } from '../../contexts/LoggedInUserContext';
-import { Card, Media } from 'react-bootstrap';
-import { Link, useHistory } from 'react-router-dom';
+import React from "react";
+import { useLoggedInUser } from "../../contexts/LoggedInUserContext";
+import { Card, Media } from "react-bootstrap";
+import { Link, useHistory } from "react-router-dom";
 import appStyles from "../../App.module.css";
-import { DropdownMenu } from '../../components/DropdownMenu';
-import { axiosRes } from '../../api/AxiosDefaults';
-
+import { DropdownMenu } from "../../components/DropdownMenu";
+import { axiosRes } from "../../api/AxiosDefaults";
 
 const Artwork = (props) => {
     const {
         id,
         owner,
-        artwork_title,       
-        artist_name, 
+        artwork_title,
+        artist_name,
         style,
         type,
         payment_method,
         price,
         image,
-        alt_text, 
+        alt_text,
         contact,
         location,
         description,
@@ -34,7 +33,7 @@ const Artwork = (props) => {
 
     const handleEdit = () => {
         history.push(`/artworks/${id}/edit`);
-    }
+    };
 
     const handleDelete = async () => {
         try {
@@ -48,17 +47,18 @@ const Artwork = (props) => {
     return (
         <Card>
             <Card.Body>
-                <Media className='align-items-center justify-content-between'>
+                <Media className="align-items-center justify-content-between">
                     {owner}
                 </Media>
-                <div className='d-flex align-items-center'>
+                <div className="d-flex align-items-center">
                     <span>{updated_at}</span>
-                    {is_owner && artworkPage && <DropdownMenu
-                        handleEdit={handleEdit}
-                        handleDelete={handleDelete}
-                        /> 
-                    }
-                </div>
+                    {is_owner && artworkPage && (
+                        <DropdownMenu
+                            handleEdit={handleEdit}
+                            handleDelete={handleDelete}
+                        />
+                    )}
+                    </div>
             </Card.Body>
 
             <Link to={`/artworks/${id}`}>
@@ -68,36 +68,38 @@ const Artwork = (props) => {
                     className={appStyles.Image}
                 />
             </Link>
+
             <Card.Body>
                 {sold ? <p>Sold</p> : null}
-                {artwork_title && <Card.Title className='text-center'>
+                {artwork_title && (
+                <Card.Title className="text-center">
                     {artwork_title}
                 </Card.Title>
-                }
-                
+                )}
+
                 {artist_name && <Card.Text>
                     Artist name: {artist_name}
-                    </Card.Text>
+                </Card.Text>
                 }
                 <div>
                     <p>Bid count: {bids_count}</p>
                 </div>
                 {style && <Card.Text>Style: {style}</Card.Text>}
                 {type && <Card.Text>Type: {type}</Card.Text>}
-                {payment_method && <Card.Text>
-                    Payment method: {payment_method}
-                    </Card.Text>
-                }
+                {payment_method && (
+                <Card.Text>Payment method: {payment_method}</Card.Text>
+                )}
                 {price && <Card.Text>Price: {price}</Card.Text>}
                 {contact && <Card.Text>Contact: {contact}</Card.Text>}
                 {location && <Card.Text>Location: {location}</Card.Text>}
-                {description && <Card.Text>
-                    Description: {description}
+                {description &&
+                    <Card.Text>
+                        Description: {description}
                     </Card.Text>
                 }
             </Card.Body>
         </Card>
-    )
-}
+    );
+    };
 
 export default Artwork;
