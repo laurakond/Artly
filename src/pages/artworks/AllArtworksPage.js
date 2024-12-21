56372ae65b453a6334f99363247e5116ae3b604e
Laura Kondrataite <laura.kondrataite@gmail.com>
2024-12-21 10:19:15 +0000

    pages(add filters by style and type)

--- src/pages/artworks/AllArtworksPage.js
+++ src/pages/artworks/AllArtworksPage.js
@@ -10,10 +10,13 @@ import { axiosReq } from '../../api/AxiosDefaults';
 import Artwork from './Artwork';
 import Asset from '../../components/Asset';
 
-const AllArtworksPage = ({message}) => {
+const AllArtworksPage = ({message, filter=""}) => {
     const [artworks, setArtworks] = useState({results: []});
     const [hasLoaded, setHasLoaded] = useState(false); 
     const [searchQuery, setSearchQuery] = useState("");
+    // search for style/type
+    const [styleQuery, setStyleQuery] = useState("");
+    const [typeQuery, setTypeQuery] = useState("");
 
     useEffect(() => {
         const fetchArtworks = async () => {
@@ -29,11 +32,42 @@ const AllArtworksPage = ({message}) => {
         const timer = setTimeout(() => {
             fetchArtworks();
             }, 1000);
-    
+
             return () => {
             clearTimeout(timer);
             };
     }, [searchQuery]);
+
+    // useEffect for filtering based on the style category. 
+    useEffect(() => {
+        const fetchStyles = async () => {
+            try {
+                const {data} = await axiosReq.get(`/artworks/?${filter}search=${styleQuery}`);
+                setArtworks(data);
+                setHasLoaded(true);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        setHasLoaded(false);
+        fetchStyles();
+    }, [filter, styleQuery]);
+
+    // useEffect for filtering based on the type category. 
+    useEffect(() => {
+        const fetchType = async () => {
+            try {
+                const {data} = await axiosReq.get(`/artworks/?${filter}search=${typeQuery}`);
+                setArtworks(data);
+                setHasLoaded(true);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        setHasLoaded(false);
+        fetchType();
+    }, [filter, typeQuery]);
 
     return (
         <Row className="h-100">
@@ -49,6 +83,53 @@ const AllArtworksPage = ({message}) => {
                     placeholder="Search artworks"
                 />
             </Form>
+            {/* Search for styles/types of artwork */}
+            <Form onSubmit={(event) => event.preventDefault()}>
+                <Form.Label >
+                    View by Style
+                </Form.Label>
+                <Form.Control
+                    value={styleQuery}
+                    onChange={(event) => setStyleQuery(event.target.value)}
+                    type="text"
+                    className="mr-sm-2"
+                    as="select"
+                >
+                    <option value="">Select artwork style</option>
+                    <option value="Modern">Modern</option>
+                    <option value="Contemporary">Contemporary</option>
+                    <option value="Digital art">Digital art</option>
+                    <option value="Old Masters">Old Masters</option>
+                    <option value="Classical">Classical</option>
+                    <option value="Other">Other</option>
+                </Form.Control>
+            </Form>
+
+            <Form onSubmit={(event) => event.preventDefault()}>
+                <Form.Label >
+                    View by Type
+                </Form.Label>
+                <Form.Control
+                    value={typeQuery}
+                    onChange={(event) => setTypeQuery(event.target.value)}
+                    type="text"
+                    className="mr-sm-2"
+                    as="select"
+                >
+                    <option value="">Select artwork type</option>
+                    <option value="Collage">Collage</option>
+                    <option value="Drawing">Drawing</option>
+                    <option value="Needlework">Needlework</option>
+                    <option value="Etching">Etching</option>
+                    <option value="Painting">Painting</option>
+                    <option value="Photography">Photography</option>
+                    <option value="Pottery">Pottery</option>
+                    <option value="Sculpture">Scultpure</option>
+                    <option value="Watercolour">Watercolour</option>
+                    <option value="Other">Other</option>
+                </Form.Control>
+            </Form>
+
                 {hasLoaded ? (
                     <>
                         {artworks.results.length ? (
@@ -74,4 +155,4 @@ const AllArtworksPage = ({message}) => {
     );
 }
 
-export default AllArtworksPage
+export default AllArtworksPage;
