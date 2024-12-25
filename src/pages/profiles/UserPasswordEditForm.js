90d1746abd46e8da043fd4a4a0b2515bb49efb12
Laura Kondrataite <laura.kondrataite@gmail.com>
2024-12-25 10:06:26 +0000

    pages(add forms for editing user profile)

---
+++ src/pages/profiles/UserPasswordEditForm.js
@@ -0,0 +1,96 @@
+import React, { useEffect, useState } from "react";
+
+import Alert from "react-bootstrap/Alert";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+
+import { useHistory, useParams } from "react-router-dom";
+import { axiosRes } from "../../api/AxiosDefaults";
+import { useLoggedInUser } from "../../contexts/LoggedInUserContext";
+
+const UserPasswordEditForm = () => {
+  const history = useHistory();
+  const { id } = useParams();
+  const loggedInUser = useLoggedInUser();
+
+  const [userData, setUserData] = useState({
+    new_password1: "",
+    new_password2: "",
+  });
+  const { new_password1, new_password2 } = userData;
+
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (event) => {
+    setUserData({
+      ...userData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  useEffect(() => {
+    if (loggedInUser?.profile_id?.toString() !== id) {
+      // redirect user if they are not the owner of this profile
+      history.push("/");
+    }
+  }, [loggedInUser, history, id]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axiosRes.post("/dj-rest-auth/password/change/", userData);
+      history.goBack();
+    } catch (error) {
+      console.log(error);
+      setErrors(error.response?.data);
+    }
+  };
+
+  return (
+    <Row>
+      <Col className="py-2 mx-auto text-center" md={6}>
+        <Container>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group>
+              <Form.Label>New password</Form.Label>
+              <Form.Control
+                placeholder="new password"
+                type="password"
+                value={new_password1}
+                onChange={handleChange}
+                name="new_password1"
+              />
+            </Form.Group>
+            {errors?.new_password1?.map((message, index) => (
+              <Alert key={index} variant="warning">
+                {message}
+              </Alert>
+            ))}
+            <Form.Group>
+              <Form.Label>Confirm password</Form.Label>
+              <Form.Control
+                placeholder="confirm new password"
+                type="password"
+                value={new_password2}
+                onChange={handleChange}
+                name="new_password2"
+              />
+            </Form.Group>
+            {errors?.new_password2?.map((message, index) => (
+              <Alert key={index} variant="warning">
+                {message}
+              </Alert>
+            ))}
+            <Button onClick={() => history.goBack()}>cancel</Button>
+            <Button type="submit">save</Button>
+          </Form>
+        </Container>
+      </Col>
+    </Row>
+  );
+};
+
+export default UserPasswordEditForm;
