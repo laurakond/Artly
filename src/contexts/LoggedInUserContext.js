b36a2f6edcc91538bf6438d7ab799bd9b85a006c
Laura Kondrataite <laura.kondrataite@gmail.com>
2024-12-25 09:37:43 +0000

    tidyup(format code)

--- src/contexts/LoggedInUserContext.js
+++ src/contexts/LoggedInUserContext.js
@@ -1,7 +1,7 @@
-import { createContext, useContext, useEffect, useMemo, useState } from 'react';
-import axios from 'axios';
-import { axiosReq, axiosRes } from '../api/AxiosDefaults';
-import { useHistory } from 'react-router';
+import { createContext, useContext, useEffect, useMemo, useState } from "react";
+import axios from "axios";
+import { axiosReq, axiosRes } from "../api/AxiosDefaults";
+import { useHistory } from "react-router";
 
 export const LoggedInUserContext = createContext();
 export const SetLoggedInUserContext = createContext();
@@ -9,70 +9,70 @@ export const SetLoggedInUserContext = createContext();
 export const useLoggedInUser = () => useContext(LoggedInUserContext);
 export const useSetLoggedInUser = () => useContext(SetLoggedInUserContext);
 
-export const LoggedInUserProvider = ({children}) => {
-    const [loggedInUser, setLoggedInUser] = useState(null);
-    const history = useHistory();
+export const LoggedInUserProvider = ({ children }) => {
+  const [loggedInUser, setLoggedInUser] = useState(null);
+  const history = useHistory();
 
-    const handleMount = async () => {
+  const handleMount = async () => {
+    try {
+      const { data } = await axiosRes.get("dj-rest-auth/user/");
+      setLoggedInUser(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    handleMount();
+  }, []);
+
+  useMemo(() => {
+    axiosReq.interceptors.request.use(
+      async (config) => {
         try {
-            const {data} = await axiosRes.get('dj-rest-auth/user/')
-            setLoggedInUser(data)
-        } catch (error) {
-            console.log(error);
+          await axios.post("/dj-rest-auth/token/refresh/");
+        } catch (err) {
+          setLoggedInUser((prevLoggedInUser) => {
+            if (prevLoggedInUser) {
+              history.push("/signin");
+            }
+            return null;
+          });
+          return config;
         }
-    };
+        return config;
+      },
+      (err) => {
+        return Promise.reject(err);
+      }
+    );
 
-    useEffect(()=>{
-        handleMount()
-    }, []);
+    axiosRes.interceptors.response.use(
+      (response) => response,
+      async (err) => {
+        if (err.response?.status === 401) {
+          try {
+            await axios.post("/dj-rest-auth/token/refresh/");
+          } catch (err) {
+            setLoggedInUser((prevLoggedInUser) => {
+              if (prevLoggedInUser) {
+                history.push("/signin");
+              }
+              return null;
+            });
+          }
+          return axios(err.config);
+        }
+        return Promise.reject(err);
+      }
+    );
+  }, [history]);
 
-    useMemo(() => {
-        axiosReq.interceptors.request.use(
-            async (config) => {
-                try {
-                await axios.post("/dj-rest-auth/token/refresh/");
-                } catch (err) {
-                setLoggedInUser((prevLoggedInUser) => {
-                    if (prevLoggedInUser) {
-                    history.push("/signin");
-                    }
-                    return null;
-                });
-                return config;
-                }
-                return config;
-            },
-            (err) => {
-                return Promise.reject(err);
-            }
-            );
-
-            axiosRes.interceptors.response.use(
-            (response) => response,
-            async (err) => {
-                if (err.response?.status === 401) {
-                try {
-                    await axios.post("/dj-rest-auth/token/refresh/");
-                } catch (err) {
-                    setLoggedInUser((prevLoggedInUser) => {
-                    if (prevLoggedInUser) {
-                        history.push("/signin");
-                    }
-                    return null;
-                    });
-                }
-                return axios(err.config);
-                }
-                return Promise.reject(err);
-            }
-            );
-        }, [history]);
-
-    return(
-        <LoggedInUserContext.Provider value={loggedInUser}>
-            <SetLoggedInUserContext.Provider value={setLoggedInUser}>
-                {children}
-            </SetLoggedInUserContext.Provider>
-        </LoggedInUserContext.Provider>
-    )
-}
+  return (
+    <LoggedInUserContext.Provider value={loggedInUser}>
+      <SetLoggedInUserContext.Provider value={setLoggedInUser}>
+        {children}
+      </SetLoggedInUserContext.Provider>
+    </LoggedInUserContext.Provider>
+  );
+};
