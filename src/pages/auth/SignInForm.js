bfeb5ba8b43f37833b461456a2f291d80a95585f
Laura Kondrataite <laura.kondrataite@gmail.com>
2024-12-27 14:36:21 +0000

    notifications(add toast messages)

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
@@ -9,102 +9,99 @@ import Alert from "react-bootstrap/Alert";
 import { Link, useHistory } from "react-router-dom";
 import axios from "axios";
 import { useSetLoggedInUser } from "../../contexts/LoggedInUserContext";
+import { toast } from "react-toastify";
 
 function SignInForm() {
-    const setLoggedInUser = useSetLoggedInUser();
-    const [signInData, setSignInData] = useState({
-        username: "",
-        password: "",
+  const setLoggedInUser = useSetLoggedInUser();
+  const [signInData, setSignInData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const { username, password } = signInData;
+  const [errors, setErrors] = useState({});
+  const history = useHistory();
+
+  const handleChange = (event) => {
+    setSignInData({
+      ...signInData,
+      [event.target.name]: event.target.value,
     });
+  };
 
-    const {username, password} = signInData;
-    const [errors, setErrors] = useState({});
-    const history = useHistory();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await axios.post("/dj-rest-auth/login/", signInData);
+      setLoggedInUser(data.user);
+      history.push("/");
+      toast.success("You are signed in!");
+    } catch (error) {
+      setErrors(error.response?.data);
+      toast.error(
+        "Something went wrong while attempting to sign in. Please try again."
+      );
+    }
+  };
 
-    const handleChange = (event) => {
-        setSignInData({
-            ...signInData,
-            [event.target.name]: event.target.value,
-        });
-    };
+  return (
+    <Row>
+      <Col className="my-auto p-0 p-md-2" md={6}>
+        <Container>
+          <h1>Sign In</h1>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="username">
+              <Form.Label className="d-none">Username</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Username"
+                name="username"
+                value={username}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {errors.username?.map((message, index) => (
+              <Alert variant="warning" key={index}>
+                {message}
+              </Alert>
+            ))}
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        try {
-            const {data} = await axios.post('/dj-rest-auth/login/', signInData);
-            setLoggedInUser(data.user);
-            history.push('/');
-        } catch (error) {
-            setErrors(error.response?.data)
-        }
-    };
+            <Form.Group controlId="password">
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={password}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {errors.password?.map((message, index) => (
+              <Alert variant="warning" key={index}>
+                {message}
+              </Alert>
+            ))}
 
-    return (
-        <Row>
-        <Col className="my-auto p-0 p-md-2" md={6}>
-            <Container>
-            <h1 >Sign In</h1>
-            <Form onSubmit={handleSubmit}>
-                <Form.Group controlId="username">
-                    <Form.Label className="d-none">
-                        Username
-                    </Form.Label>
-                    <Form.Control 
-                        type="text" 
-                        placeholder="Username"
-                        name="username"
-                        value={username}
-                        onChange={handleChange}
-                    />
-                </Form.Group>
-                {errors.username?.map((message, index) => (
-                    <Alert variant="warning" key={index}>
-                        {message}
-                    </Alert>
-                ))}
-
-                <Form.Group controlId="password">
-                    <Form.Label className="d-none">
-                        Password
-                    </Form.Label>
-                    <Form.Control 
-                        type="password" 
-                        placeholder="Password" 
-                        name="password"
-                        value={password}
-                        onChange={handleChange}
-                    />
-                </Form.Group>
-                {errors.password?.map((message, index) => (
-                    <Alert variant="warning" key={index}>
-                        {message}
-                    </Alert>
-                ))}
-
-                <Button type="submit">
-                    Sign In
-                </Button>
-                {errors.non_field_errors?.map((message, index) => (
-                    <Alert variant="warning" key={index}>
-                        {message}
-                    </Alert>
-                ))}
-            </Form>
+            <Button type="submit">Sign In</Button>
+            {errors.non_field_errors?.map((message, index) => (
+              <Alert variant="warning" key={index}>
+                {message}
+              </Alert>
+            ))}
+          </Form>
         </Container>
         <Container>
-            <Link to="/signup">
-                Don't have an account? 
-                <span>
-                    Sign up now!
-                </span>
-            </Link>
-            </Container>
-        </Col>
-        <Col md={6}>
-            <Image/>
-        </Col>
-        </Row>
-    );
+          <Link to="/signup">
+            Don't have an account?
+            <span>Sign up now!</span>
+          </Link>
+        </Container>
+      </Col>
+      <Col md={6}>
+        <Image />
+      </Col>
+    </Row>
+  );
 }
 
 export default SignInForm;
--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,107 +1,123 @@
 import React, { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
-import { Form, Button, Image, Col, Row, Container, Alert } from "react-bootstrap";
+import {
+  Form,
+  Button,
+  Image,
+  Col,
+  Row,
+  Container,
+  Alert,
+} from "react-bootstrap";
 import axios from "axios";
-
+import { toast } from "react-toastify";
 
 const SignUpForm = () => {
+  const [signUpData, setSignUpData] = useState({
+    username: "",
+    password1: "",
+    password2: "",
+  });
 
-    const [signUpData, setSignUpData] = useState({
-        username: "",
-        password1: "",
-        password2: "",
+  const { username, password1, password2 } = signUpData;
+  const [errors, setErrors] = useState({});
+  const history = useHistory();
+
+  const handleChange = (event) => {
+    setSignUpData({
+      ...signUpData,
+      [event.target.name]: event.target.value,
     });
+  };
 
-    const {username, password1, password2} = signUpData;
-    const [errors, setErrors] = useState({});
-    const history = useHistory();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post("/dj-rest-auth/registration/", signUpData);
+      history.push("/signin");
+      toast.success("You are signed up! Please sing in.");
+    } catch (error) {
+      setErrors(error.response?.data);
+      toast.error(
+        "Something went wrong while attempting to sign up. Please try again."
+      );
+    }
+  };
 
-    const handleChange = (event) => {
-        setSignUpData({
-            ...signUpData,
-            [event.target.name]: event.target.value,
-        });
-    };
+  return (
+    <Row>
+      <Col className="my-auto py-2 p-md-2" md={6}>
+        <Container>
+          <h1>Sign Up</h1>
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        try {
-            await axios.post('/dj-rest-auth/registration/', signUpData);
-            history.push('/signin');
-        } catch (error) {
-            setErrors(error.response?.data)
-        }
-    };
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="username">
+              <Form.Label className="d-none">username</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="username"
+                name="username"
+                value={username}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {errors.username?.map((message, index) => (
+              <Alert variant="warning" key={index}>
+                {message}
+              </Alert>
+            ))}
 
-    return (
-        <Row>
-        <Col className="my-auto py-2 p-md-2" md={6}>
-            <Container>
-            <h1>Sign Up</h1>
+            <Form.Group controlId="passwrod1">
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                name="password1"
+                value={password1}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {errors.password1?.map((message, index) => (
+              <Alert variant="warning" key={index}>
+                {message}
+              </Alert>
+            ))}
 
-            <Form onSubmit={handleSubmit}>
-                <Form.Group controlId="username">
-                    <Form.Label className="d-none">username</Form.Label>
-                    <Form.Control
-                        type="text"
-                        placeholder="username"
-                        name="username"
-                        value={username}
-                        onChange={handleChange}
-                    />
-                </Form.Group>
-                {errors.username?.map((message, index) => (
-                    <Alert variant="warning" key={index}>{message}</Alert>
-                ))}
+            <Form.Group controlId="passwrod2">
+              <Form.Label className="d-none">Confirm password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Confirm password"
+                name="password2"
+                value={password2}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {errors.password2?.map((message, index) => (
+              <Alert variant="warning" key={index}>
+                {message}
+              </Alert>
+            ))}
 
-                <Form.Group controlId="passwrod1">
-                    <Form.Label className="d-none">Password</Form.Label>
-                    <Form.Control
-                        type="password"
-                        placeholder="Password"
-                        name="password1"
-                        value={password1}
-                        onChange={handleChange}
-                    />
-                </Form.Group>
-                {errors.password1?.map((message, index) => (
-                    <Alert variant="warning" key={index}>{message}</Alert>
-                ))}
-
-                <Form.Group controlId="passwrod2">
-                    <Form.Label className="d-none">Confirm password</Form.Label>
-                    <Form.Control
-                        type="password"
-                        placeholder="Confirm password"
-                        name="password2"
-                        value={password2}
-                        onChange={handleChange}
-                    />
-                </Form.Group>
-                {errors.password2?.map((message, index) => (
-                    <Alert variant="warning" key={index}>{message}</Alert>
-                ))}
-
-                <Button type="submit">
-                    Sign up
-                </Button>
-                {errors.non_field_errors?.map((message, index) => (
-                    <Alert variant="warning" key={index}>{message}</Alert>
-                ))}
-            </Form>
-
-            </Container>
-            <Container >
-            <Link  to="/signin">
-                Already have an account? <span>Sign in</span>
-            </Link>
-            </Container>
-        </Col>
-        <Col>
-            <Image/>
-        </Col>
-        </Row>
-    );
+            <Button type="submit">Sign up</Button>
+            {errors.non_field_errors?.map((message, index) => (
+              <Alert variant="warning" key={index}>
+                {message}
+              </Alert>
+            ))}
+          </Form>
+        </Container>
+        <Container>
+          <Link to="/signin">
+            Already have an account? <span>Sign in</span>
+          </Link>
+        </Container>
+      </Col>
+      <Col>
+        <Image />
+      </Col>
+    </Row>
+  );
 };
 
 export default SignUpForm;
