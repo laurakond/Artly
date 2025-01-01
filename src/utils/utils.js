a97ed80c7ea985947d3aaf1ca5ee563674145dce
Laura Kondrataite <laura.kondrataite@gmail.com>
2025-01-01 17:40:55 +0000

    untils(add unfollow user functionality)

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -17,7 +17,7 @@ export const fetchMoreData = async (resource, setResource) => {
   }
 };
 
-export const followHelper = (profile, clickedProfile, following_id) => {
+export const followUserHelper = (profile, clickedProfile, following_id) => {
   return profile.id === clickedProfile.id
     ? // this is the profile I clicked on update its followers count and set its following id
       {
@@ -30,6 +30,24 @@ export const followHelper = (profile, clickedProfile, following_id) => {
       {
         ...profile,
         following_count: profile.following_count + 1,
+      }
+    : // this is not the profile the user clicked on or the profile the user owns, so just return it unchanged
+      profile;
+};
+
+export const unfollowUserHelper = (profile, clickedProfile) => {
+  return profile.id === clickedProfile.id
+    ? // thisis the profile I clicked on update its followers count and set its following id
+      {
+        ...profile,
+        followers_count: profile.followers_count - 1,
+        following_id: null,
+      }
+    : profile.is_owner
+    ? // this is the profile of the logged in user update its following count
+      {
+        ...profile,
+        following_count: profile.following_count - 1,
       }
     : // this is not the profile the user clicked on or the profile the user owns, so just return it unchanged
       profile;
