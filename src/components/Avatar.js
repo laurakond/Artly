273659368c6946c5d18544a4714a7656e933a472
Laura Kondrataite <laura.kondrataite@gmail.com>
2025-01-02 09:12:56 +0000

    components(create Avatar component)

---
+++ src/components/Avatar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "../styles/Avatar.module.css";
+
+// This code snippet was taken from Code Institute's Moments walkthrough, which is noted in the README.md
+const Avatar = ({ src, height = 45, text }) => {
+  return (
+    <span>
+      <img
+        className={styles.Avatar}
+        src={src}
+        height={height}
+        width={height}
+        alt="avatar"
+      />
+      {text}
+    </span>
+  );
+};
+
+export default Avatar;
