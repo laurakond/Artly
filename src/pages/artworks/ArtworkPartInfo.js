54b71d9eaab5b0e87374d086eefbcf17ad4a45c4
Laura Kondrataite <laura.kondrataite@gmail.com>
2025-01-05 14:20:06 +0000

    pages(apply car styles)

--- src/pages/artworks/ArtworkPartInfo.js
+++ src/pages/artworks/ArtworkPartInfo.js
@@ -7,8 +7,8 @@ import Col from "react-bootstrap/Col";
 import CardImg from "react-bootstrap/CardImg";
 import { Link } from "react-router-dom";
 import appStyles from "../../App.module.css";
-import { useLoggedInUser } from "../../contexts/LoggedInUserContext";
 import styles from "../../styles/Artwork.module.css";
+import { useLoggedInUser } from "../../contexts/LoggedInUserContext";
 import { OverlayTrigger } from "react-bootstrap";
 import { Tooltip } from "react-bootstrap";
 import { axiosRes } from "../../api/AxiosDefaults";
@@ -80,55 +80,74 @@ const ArtworkPartInfo = (props) => {
   };
 
   return (
-    <Card className={styles.Parent}>
-      <div>{sold ? <span className={styles.Ribbon}>Sold</span> : null}</div>
-
-      <Card.Body style={{ opacity: sold ? "60%" : null }}>
-        <Media className="align-items-center justify-content-between"></Media>
-        <Link to={`/profiles/${profile_id}`}>
-          <Avatar src={profile_image} height={55} alt={`${owner}'s avatar`} />
-          {owner}
-        </Link>
-
-        <div className="d-flex align-items-center">
-          <span>{updated_at}</span>
-        </div>
-
-        <Link to={`/artworks/${id}`}>
-          <div className="text-center">
-            {artwork_title && <Card.Title>{artwork_title}</Card.Title>}
-            {artist_name && <Card.Text>By {artist_name}</Card.Text>}
-            {description && <Card.Text>Description: {description}</Card.Text>}
+    <Card
+      className={`p-0 ${styles.Parent}`}
+      lg={8}
+      style={{ maxWidth: "18rem" }}
+    >
+      {sold ? <span className={styles.Ribbon}>Sold</span> : null}
+      <CardImg
+        variant="top"
+        src={image}
+        alt={alt_text}
+        className={styles.CardImageStyle}
+      />
+      <Card.Header className={`${styles.CardHeader}`}>
+        <div
+          className={`d-flex flex-direction-row justify-content-between align-items-center`}
+        >
+          {/* <Media> */}
+          <div>
+            <Link to={`/profiles/${profile_id}`}>
+              <Avatar
+                src={profile_image}
+                height={40}
+                alt={`${owner}'s avatar`}
+              />
+            </Link>
+            {/* </Media> */}
+            {owner}
           </div>
-
-          <Container className="p-4">
-            <Row>
-              <Col>
-                <CardImg
-                  src={image}
-                  alt={alt_text}
-                  className={appStyles.Image}
-                />
-              </Col>
-            </Row>
-
-            <Row>
-              <Col>
-                {style && <Card.Text>Style: {style}</Card.Text>}
-                {type && <Card.Text>Type: {type}</Card.Text>}
-              </Col>
-              <Col>
-                {price && <Card.Text>Price: £{price}</Card.Text>}
-                {bids_count === 0 ? (
-                  <Card.Text>Number of bids: No bids yet</Card.Text>
-                ) : (
-                  <Card.Text>Number of bids: {bids_count}</Card.Text>
-                )}
-              </Col>
-            </Row>
-          </Container>
+          <div>{updated_at}</div>
+        </div>
+      </Card.Header>
+      <Card.Body>
+        <Link to={`/artworks/${id}`}>
+          {artwork_title && (
+            <Card.Title className={`${styles.CardTitle}`}>
+              {artwork_title}
+            </Card.Title>
+          )}
+          {artist_name && (
+            <Card.Text className={`${styles.CardText}`}>
+              By {artist_name}
+            </Card.Text>
+          )}
+          {description && (
+            <Card.Text className={`${styles.CardText}`}>
+              {description}
+            </Card.Text>
+          )}
         </Link>
-        <Col>
+      </Card.Body>
+      <Card.Footer className={`${styles.CardFooter}`}>
+        <div className={`d-flex flex-direction-row justify-content-around`}>
+          {price && (
+            <div>
+              <i class="fa-solid fa-hand-holding-dollar"></i> £{price}
+            </div>
+          )}
+
+          {bids_count === 0 ? (
+            <div>
+              <i class="fa-solid fa-gavel"></i> 0
+            </div>
+          ) : (
+            <div>
+              <i class="fa-solid fa-gavel"></i> {bids_count}
+            </div>
+          )}
+
           <div>
             {is_owner ? (
               <OverlayTrigger
@@ -155,9 +174,80 @@ const ArtworkPartInfo = (props) => {
             )}
             {saved_count}
           </div>
-        </Col>
-      </Card.Body>
+        </div>
+      </Card.Footer>
     </Card>
+
+    // <Card className={`p-0 ${styles.Parent}`} lg={8}>
+    //   <div>{sold ? <span className={styles.Ribbon}>Sold</span> : null}</div>
+
+    //   <Card.Body style={{ opacity: sold ? "60%" : null }}>
+    //     {/* <Media className="align-items-center justify-content-between"></Media> */}
+
+    //     {/* <Row> */}
+    //     <Link to={`/profiles/${profile_id}`}>
+    //       <Avatar src={profile_image} height={55} alt={`${owner}'s avatar`} />
+    //       {owner}
+    //     </Link>
+    //     <div className="d-flex align-items-center">
+    //       <span>{updated_at}</span>
+    //     </div>
+    //     {/* </Row> */}
+
+    //     <Link to={`/artworks/${id}`}>
+    //       <div className="text-center">
+    //         {artwork_title && <Card.Title>{artwork_title}</Card.Title>}
+    //         {artist_name && <Card.Text>By {artist_name}</Card.Text>}
+    //         {description && <Card.Text>Description: {description}</Card.Text>}
+    //       </div>
+
+    //       {/* <Row> */}
+    //       {/* <Col> */}
+    //       <CardImg src={image} alt={alt_text} className={appStyles.Image} />
+    //       {/* </Col> */}
+    //       {/* </Row> */}
+
+    //       <Row>
+    //         <Col>{price && <Card.Text>Price: £{price}</Card.Text>}</Col>
+    //         <Col>
+    //           {bids_count === 0 ? (
+    //             <Card.Text>Number of bids: No bids yet</Card.Text>
+    //           ) : (
+    //             <Card.Text>Number of bids: {bids_count}</Card.Text>
+    //           )}
+    //         </Col>
+    //       </Row>
+    //     </Link>
+    //     <Col>
+    //       <div>
+    //         {is_owner ? (
+    //           <OverlayTrigger
+    //             placement="top"
+    //             overlay={<Tooltip>You can't save your own post!</Tooltip>}
+    //           >
+    //             <i className="fa-regular fa-bookmark" />
+    //           </OverlayTrigger>
+    //         ) : save_id ? (
+    //           <span onClick={handleDeselectSave}>
+    //             <i className="fa-solid fa-bookmark" />
+    //           </span>
+    //         ) : loggedInUser ? (
+    //           <span onClick={handleSave}>
+    //             <i className="fa-regular fa-bookmark" />
+    //           </span>
+    //         ) : (
+    //           <OverlayTrigger
+    //             placement="top"
+    //             overlay={<Tooltip>Log in to like posts!</Tooltip>}
+    //           >
+    //             <i className="fa-regular fa-bookmark" />
+    //           </OverlayTrigger>
+    //         )}
+    //         {saved_count}
+    //       </div>
+    //     </Col>
+    //   </Card.Body>
+    // </Card>
   );
 };
 
