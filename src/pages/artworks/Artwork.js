d1ea4341c75f12b8bfc4476a38d4a3c0609690bd
Laura Kondrataite <laura.kondrataite@gmail.com>
2025-01-06 21:22:28 +0000

    pages(rearrange artwork component display)

--- src/pages/artworks/Artwork.js
+++ src/pages/artworks/Artwork.js
@@ -1,18 +1,16 @@
 import React from "react";
-import { useLoggedInUser } from "../../contexts/LoggedInUserContext";
-import Card from "react-bootstrap/Card";
-import Container from "react-bootstrap/Container";
-import Media from "react-bootstrap/Media";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import CardImg from "react-bootstrap/CardImg";
 import { Link, useHistory } from "react-router-dom";
-import appStyles from "../../App.module.css";
+import { toast } from "react-toastify";
+import Card from "react-bootstrap/Card";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
+import CardImg from "react-bootstrap/CardImg";
 import { DropdownMenu } from "../../components/DropdownMenu";
 import { axiosRes } from "../../api/AxiosDefaults";
-import { toast } from "react-toastify";
-import styles from "../../styles/Artwork.module.css";
+import { useLoggedInUser } from "../../contexts/LoggedInUserContext";
 import Avatar from "../../components/Avatar";
+import sharedArtStyles from "../../styles/ArtworkPartInfo.module.css";
+import styles from "../../styles/Artwork.module.css";
 
 const Artwork = (props) => {
   const {
@@ -29,11 +27,13 @@ const Artwork = (props) => {
     location,
     description,
     sold,
-    updated_at,
     artworkPage,
     profile_id,
     profile_image,
-    // bids_count,
+    // save_id,
+    updated_at,
+    // handleDeselectSave,
+    // handleSave,
   } = props;
 
   const loggedInUser = useLoggedInUser();
@@ -58,63 +58,198 @@ const Artwork = (props) => {
   };
 
   return (
-    <Card className={styles.Parent}>
-      <div>{sold ? <span className={styles.Ribbon}>Sold</span> : null}</div>
-      <Card.Body style={{ opacity: sold ? "60%" : null }}>
-        <Media className="align-items-center justify-content-between">
-          <Link to={`/profiles/${profile_id}`}>
-            <Avatar
-              src={profile_image}
-              height={55}
-              alt={`profile image for ${owner}`}
-            />
-            {owner}
-          </Link>
-        </Media>
+    <Card
+      className={`flex-column p-0 ${sharedArtStyles.Parent}`}
+      lg={8}
+      style={{ maxWidth: "100%" }}
+    >
+      <div>
+        {sold ? <span className={sharedArtStyles.Ribbon}>Sold</span> : null}
+      </div>
+      <Link to={`/artworks/${id}`} className={`d-flex justify-content-center`}>
+        <CardImg
+          variant="top"
+          src={image}
+          alt={artwork_title}
+          className={styles.ArtworkContainImage}
+        />
+      </Link>
 
-        <div className="d-flex align-items-center">
-          <span>{updated_at}</span>
-          {is_owner && artworkPage && (
-            <DropdownMenu handleEdit={handleEdit} handleDelete={handleDelete} />
-          )}
-        </div>
-
-        {/* <Link to={`/artworks/${id}`}> */}
-        <div className="text-center">
-          {artwork_title && <Card.Title>{artwork_title}</Card.Title>}
-          {artist_name && <Card.Text>By {artist_name}</Card.Text>}
-          {description && <Card.Text>Description: {description}</Card.Text>}
-        </div>
-
-        <Container className="p-4">
-          <Row>
-            <Col>
-              <CardImg
-                src={image}
-                alt={artwork_title}
-                className={appStyles.Image}
+      <div className={`d-flex flex-column`}>
+        <Card.Header className={`${sharedArtStyles.CardHeader}`}>
+          <div className="d-flex justify-content-between align-items-center">
+            <Link to={`/profiles/${profile_id}`}>
+              <Avatar
+                src={profile_image}
+                height={40}
+                alt={`${owner}'s avatar`}
               />
-            </Col>
+              {owner}
+            </Link>
+            <div>
+              {is_owner ? (
+                artworkPage &&
+                !sold && (
+                  <DropdownMenu
+                    handleEdit={handleEdit}
+                    handleDelete={handleDelete}
+                  />
+                )
+              ) : (
+                <div>{updated_at}</div>
+              )}
+            </div>
+          </div>
+          <div
+            className={`d-flex justify-content-center align-items-center ${styles.HeaderPriceAndLocation}`}
+          >
+            <OverlayTrigger
+              placement="top"
+              overlay={<Tooltip>Artwork price</Tooltip>}
+            >
+              <div>
+                {price && (
+                  <div>
+                    <i className="fa-solid fa-hand-holding-dollar"></i> £{price}
+                  </div>
+                )}
+              </div>
+            </OverlayTrigger>
+            <div>
+              <i className="fa-solid fa-location-dot"></i> {location}
+            </div>
+          </div>
+        </Card.Header>
 
-            <Col>
-              <Card.Body>
+        <Card.Body className={`flex-grow-1 ${styles.CardBody}`}>
+          <Link to={`/artworks/${id}`}>
+            <div>
+              {artwork_title && (
+                <Card.Title className={`${sharedArtStyles.CardTitle}`}>
+                  {artwork_title}
+                </Card.Title>
+              )}
+              {artist_name && (
+                <Card.Text className={`${sharedArtStyles.CardText}`}>
+                  By {artist_name}
+                </Card.Text>
+              )}
+              {description && (
+                <Card.Text className={`${sharedArtStyles.CardText}`}>
+                  {description}
+                </Card.Text>
+              )}
+            </div>
+          </Link>
+        </Card.Body>
+
+        <Card.Footer className={`${sharedArtStyles.CardFooter} mt-auto`}>
+          <div className="d-flex justify-content-around">
+            <div>
+              <OverlayTrigger
+                placement="top"
+                overlay={<Tooltip>Artwork style</Tooltip>}
+              >
                 <div>
-                  {style && <Card.Text>Style: {style}</Card.Text>}
-                  {type && <Card.Text>Type: {type}</Card.Text>}
-                  {payment_method && (
-                    <Card.Text>Payment method: {payment_method}</Card.Text>
+                  {style && (
+                    <div>
+                      <i className="fa-solid fa-pen"></i> {style}
+                    </div>
                   )}
-                  {price && <Card.Text>Price: {price}</Card.Text>}
-                  {contact && <Card.Text>Contact: {contact}</Card.Text>}
-                  {location && <Card.Text>Location: {location}</Card.Text>}
-                  {/* <p> Bid count for testing: {bids_count}</p> */}
                 </div>
-              </Card.Body>
-            </Col>
-          </Row>
-        </Container>
-        {/* </Link> */}
-      </Card.Body>
+              </OverlayTrigger>
+              <OverlayTrigger
+                placement="top"
+                overlay={<Tooltip>Payment method</Tooltip>}
+              >
+                <div>
+                  {payment_method && (
+                    <div>
+                      <i className="fa-solid fa-coins"></i> {payment_method}
+                    </div>
+                  )}
+                </div>
+              </OverlayTrigger>
+            </div>
+            <div>
+              <OverlayTrigger
+                placement="top"
+                overlay={<Tooltip>Artwork type</Tooltip>}
+              >
+                <div>
+                  {type && (
+                    <div>
+                      <i className="fa-solid fa-palette"></i> {type}
+                    </div>
+                  )}
+                </div>
+              </OverlayTrigger>
+              <OverlayTrigger
+                placement="top"
+                overlay={<Tooltip>Seller's contact</Tooltip>}
+              >
+                <div>
+                  {contact && (
+                    <div>
+                      <i className="fa-solid fa-mobile-screen-button"></i>{" "}
+                      {contact}
+                    </div>
+                  )}
+                </div>
+              </OverlayTrigger>
+            </div>
+          </div>
+          {/* <div className={`${styles.Bookmark}`}>
+            {is_owner ? (
+              <OverlayTrigger
+                placement="top"
+                overlay={<Tooltip>You can't save your own artwork</Tooltip>}
+              >
+                <span>
+                  <i
+                    className={`fa-regular fa-bookmark ${sharedArtStyles.Bookmark}`}
+                  />
+                </span>
+              </OverlayTrigger>
+            ) : save_id ? (
+              <OverlayTrigger
+                placement="top"
+                overlay={
+                  <Tooltip>
+                    Click to remove the artwork from the saved artwork list
+                  </Tooltip>
+                }
+              >
+                <span onClick={handleDeselectSave}>
+                  <i
+                    className={`fa-solid fa-bookmark ${sharedArtStyles.Bookmark}`}
+                  />
+                </span>
+              </OverlayTrigger>
+            ) : loggedInUser ? (
+              <OverlayTrigger
+                placement="top"
+                overlay={<Tooltip>Click to save the artwork</Tooltip>}
+              >
+                <span onClick={handleSave}>
+                  <i
+                    className={`fa-regular fa-bookmark ${sharedArtStyles.Bookmark}`}
+                  />
+                </span>
+              </OverlayTrigger>
+            ) : (
+              <OverlayTrigger
+                placement="top"
+                overlay={<Tooltip>Log in to save the artwork</Tooltip>}
+              >
+                <i
+                  className={`fa-regular fa-bookmark ${sharedArtStyles.Bookmark}`}
+                />
+              </OverlayTrigger>
+            )}
+          </div> */}
+        </Card.Footer>
+      </div>
     </Card>
   );
 };
