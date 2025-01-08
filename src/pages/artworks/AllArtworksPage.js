933c8beb69497857fb1f19948997a670a498452d
Laura Kondrataite <laura.kondrataite@gmail.com>
2025-01-08 17:49:20 +0000

    filters(edit layout and style for filtering options)

--- src/pages/artworks/AllArtworksPage.js
+++ src/pages/artworks/AllArtworksPage.js
@@ -114,6 +114,7 @@ const AllArtworksPage = ({ message, filter = "" }) => {
         className={`py-2 p-3 p-lg-2 py-lg-3 ${styles.ContentWidth}`}
         md={10}
         lg={8}
+        style={{ position: "relative" }}
       >
         <Row className={`mx-auto ${styles.SearchAndFilterRowClass}`}>
           <Col xs={10} className="px-0 ">
@@ -147,65 +148,82 @@ const AllArtworksPage = ({ message, filter = "" }) => {
         </Row>
         {/* style/type/sold filter categories */}
         {showFilters && (
-          <div>
-            {/* Search for styles of artwork */}
-            <Form onSubmit={(event) => event.preventDefault()}>
-              <Form.Label>View by Style</Form.Label>
-              <Form.Control
-                value={styleQuery}
-                onChange={(event) => setStyleQuery(event.target.value)}
-                type="text"
-                className="mr-sm-2"
-                as="select"
-              >
-                <option value="">Select artwork style</option>
-                <option value="Modern">Modern</option>
-                <option value="Contemporary">Contemporary</option>
-                <option value="Digital art">Digital art</option>
-                <option value="Old Masters">Old Masters</option>
-                <option value="Classical">Classical</option>
-                <option value="Other">Other</option>
-              </Form.Control>
-            </Form>
-            {/* Search for types of artwork */}
-            <Form onSubmit={(event) => event.preventDefault()}>
-              <Form.Label>View by Type</Form.Label>
-              <Form.Control
-                value={typeQuery}
-                onChange={(event) => setTypeQuery(event.target.value)}
-                type="text"
-                className="mr-sm-2"
-                as="select"
-              >
-                <option value="">Select artwork type</option>
-                <option value="Collage">Collage</option>
-                <option value="Drawing">Drawing</option>
-                <option value="Needlework">Needlework</option>
-                <option value="Etching">Etching</option>
-                <option value="Painting">Painting</option>
-                <option value="Photography">Photography</option>
-                <option value="Pottery">Pottery</option>
-                <option value="Sculpture">Scultpure</option>
-                <option value="Watercolour">Watercolour</option>
-                <option value="Other">Other</option>
-              </Form.Control>
-            </Form>
+          <div className={styles.ParentFilterContainer}>
+            <div className={styles.MoreFilterOptions}>
+              {/* Search for styles of artwork */}
+              <Form onSubmit={(event) => event.preventDefault()}>
+                <div className={styles.StyleTypeSoldFilter}>
+                  <Form.Label>Artwork Style</Form.Label>
+                  <Form.Control
+                    value={styleQuery}
+                    onChange={(event) => {
+                      setStyleQuery(event.target.value);
+                      setShowFilters(false);
+                    }}
+                    type="text"
+                    className={styles.SelectContainer}
+                    as="select"
+                  >
+                    <option value="">Select style</option>
+                    <option value="Modern">Modern</option>
+                    <option value="Contemporary">Contemporary</option>
+                    <option value="Digital art">Digital art</option>
+                    <option value="Old Masters">Old Masters</option>
+                    <option value="Classical">Classical</option>
+                    <option value="Other">Other</option>
+                  </Form.Control>
+                </div>
+              </Form>
+              {/* Search for types of artwork */}
+              <Form onSubmit={(event) => event.preventDefault()}>
+                <div className={styles.StyleTypeSoldFilter}>
+                  <Form.Label>Artwork Type</Form.Label>
+                  <Form.Control
+                    value={typeQuery}
+                    onChange={(event) => {
+                      setTypeQuery(event.target.value);
+                      setShowFilters(false);
+                    }}
+                    type="text"
+                    className={styles.SelectContainer}
+                    as="select"
+                  >
+                    <option value="">Select type</option>
+                    <option value="Collage">Collage</option>
+                    <option value="Drawing">Drawing</option>
+                    <option value="Needlework">Needlework</option>
+                    <option value="Etching">Etching</option>
+                    <option value="Painting">Painting</option>
+                    <option value="Photography">Photography</option>
+                    <option value="Pottery">Pottery</option>
+                    <option value="Sculpture">Scultpure</option>
+                    <option value="Watercolour">Watercolour</option>
+                    <option value="Other">Other</option>
+                  </Form.Control>
+                </div>
+              </Form>
 
-            {/* Search for sold status */}
-            <Form onSubmit={(event) => event.preventDefault()}>
-              <Form.Label>View by Sold status</Form.Label>
-              <Form.Control
-                value={soldFilter}
-                onChange={(event) => setSoldFilter(event.target.value)}
-                type="text"
-                className="mr-sm-2"
-                as="select"
-              >
-                <option value="">All</option>
-                <option value="true">Sold</option>
-                <option value="false">For sale</option>
-              </Form.Control>
-            </Form>
+              {/* Search for sold status */}
+              <Form onSubmit={(event) => event.preventDefault()}>
+                <div className={styles.StyleTypeSoldFilter}>
+                  <Form.Label>Sold status</Form.Label>
+                  <Form.Control
+                    value={soldFilter}
+                    onChange={(event) => {
+                      setSoldFilter(event.target.value);
+                      setShowFilters(false);
+                    }}
+                    type="text"
+                    className={styles.SelectContainer}
+                    as="select"
+                  >
+                    <option value="">All</option>
+                    <option value="true">Sold</option>
+                    <option value="false">For sale</option>
+                  </Form.Control>
+                </div>
+              </Form>
+            </div>
           </div>
         )}
         {/* Artwork list view of spinner */}
