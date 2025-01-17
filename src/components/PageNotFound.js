e34f5d087a44a070aa9259b8d15884adc98cf183
Laura Kondrataite <laura.kondrataite@gmail.com>
2025-01-17 11:41:12 +0000

    layout(edit page not found layout and styles)

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,19 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import NoResults from "../assets/no-results.webp";
 import Asset from "./Asset";
 import styles from "../styles/PageNotFound.module.css";
+import appStyles from "../App.module.css";
+import Container from "react-bootstrap/Container";
 
 //This code was appropriated from Code Institute's Moments walkthrough project.
 const PageNotFound = () => {
   return (
-    <div>
+    <Container>
       <div className={`${styles.PageNotFound} text-align-center`}>
         <Asset
           src={NoResults}
-          message="Oops! Something went wrong. The page you're trying to access does not exist!"
+          message="Oops! The page you're trying to access does not exist."
         />
       </div>
-    </div>
+      <Link to="/">
+        <div className="text-center">
+          Click{" "}
+          <span
+            className={`${appStyles.AccentFont} ${appStyles.AccentFontColor} ${appStyles.HoverEffect}`}
+          >
+            here
+          </span>{" "}
+          to go back to Home page.
+        </div>
+      </Link>
+    </Container>
   );
 };
 
