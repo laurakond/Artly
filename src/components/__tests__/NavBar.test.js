6ea8b906d5629390c5f84249a37dd05a28baa9c3
Laura Kondrataite <laura.kondrataite@gmail.com>
2025-01-20 11:27:07 +0000

    testing(prepare and add automated tests for navbar links)

--- src/components/__tests__/NavBar.test.js
+++ src/components/__tests__/NavBar.test.js
@@ -3,34 +3,86 @@ import { BrowserRouter as Router } from "react-router-dom";
 import NavBar from "../NavBar";
 import { LoggedInUserProvider } from "../../contexts/LoggedInUserContext";
 
-test('renders NavBar', () =>{
-    render(
-        <Router>
-            <NavBar/>
-        </Router>
-    );
+test("renders NavBar", () => {
+  render(
+    <Router>
+      <NavBar />
+    </Router>
+  );
 
-    // screen.debug(); //works like clg
-    const signInLink = screen.getByRole('link', {name: 'Sign in'})
-    expect(signInLink).toBeInTheDocument();
+  const signInLink = screen.getByRole("link", { name: "Sign in" });
+  expect(signInLink).toBeInTheDocument();
 });
 
-test('renders sign in and sign up buttons on logout',  async () =>{
-    render(
-        <Router>
-            <LoggedInUserProvider>
-                <NavBar/>
-            </LoggedInUserProvider>
-        </Router>
-    );
+test("renders link to the user profile for a logged in user", async () => {
+  render(
+    <Router>
+      <LoggedInUserProvider>
+        <NavBar />
+      </LoggedInUserProvider>
+    </Router>
+  );
 
-    const signOutLink = await screen.findByRole("link", { name: "Sign out" });
-    fireEvent.click(signOutLink);
+  const profileAvatar = await screen.findByRole("img", {
+    name: "avatar",
+  });
+  expect(profileAvatar).toBeInTheDocument();
+});
 
-    const signInLink = await screen.findByRole("link", { name: "Sign in" });
-    const signUpLink = await screen.findByRole("link", { name: "Sign up" });
+test("renders sign in and sign up buttons on logout", async () => {
+  render(
+    <Router>
+      <LoggedInUserProvider>
+        <NavBar />
+      </LoggedInUserProvider>
+    </Router>
+  );
 
-    expect(signInLink).toBeInTheDocument();
-    expect(signUpLink).toBeInTheDocument();
-    screen.debug();
+  const signOutLink = await screen.findByRole("link", { name: "Sign out" });
+  fireEvent.click(signOutLink);
+
+  const signInLink = await screen.findByRole("link", { name: "Sign in" });
+  const signUpLink = await screen.findByRole("link", { name: "Sign up" });
+
+  expect(signInLink).toBeInTheDocument();
+  expect(signUpLink).toBeInTheDocument();
+});
+
+test("renders link to the artwork create form", async () => {
+  render(
+    <Router>
+      <LoggedInUserProvider>
+        <NavBar />
+      </LoggedInUserProvider>
+    </Router>
+  );
+
+  const listArt = await screen.findByText("List Art");
+  expect(listArt).toBeInTheDocument();
+});
+
+test("renders link to the saved artworks list view", async () => {
+  render(
+    <Router>
+      <LoggedInUserProvider>
+        <NavBar />
+      </LoggedInUserProvider>
+    </Router>
+  );
+
+  const savedArtworksList = await screen.findByText("Saved artworks");
+  expect(savedArtworksList).toBeInTheDocument();
+});
+
+test("renders link to the followed profiles view", async () => {
+  render(
+    <Router>
+      <LoggedInUserProvider>
+        <NavBar />
+      </LoggedInUserProvider>
+    </Router>
+  );
+
+  const followedProfilesList = await screen.findByText("Followed profiles");
+  expect(followedProfilesList).toBeInTheDocument();
 });
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -3,18 +3,21 @@ import { rest } from "msw";
 const baseURL = "https://artly-api-a39d790259f4.herokuapp.com/";
 
 export const handlers = [
-    rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
-        return rest(
-        ctx.json({
-            pk: 2,
-            username: "testuser",
-            email: "",
-            first_name: "",
-            last_name: "",
-        })
-        );
-    }),
-    rest.post(`${baseURL}dj-rest-auth/user/logout/`, (req, res, ctx)=>{
-        return res(ctx.status(200));
-    })
+  rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
+    return res(
+      ctx.json({
+        pk: 3,
+        username: "LauraK",
+        email: "",
+        first_name: "",
+        last_name: "",
+        profile_id: 3,
+        profile_image:
+          "https://res.cloudinary.com/dj9vdxwmb/image/upload/v1/media/images/IMG_8561_dcpqal",
+      })
+    );
+  }),
+  rest.post(`${baseURL}dj-rest-auth/logout/`, (req, res, ctx) => {
+    return res(ctx.status(200));
+  }),
 ];
